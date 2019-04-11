d353c91ac27d09aed40e7ba312abfe60aee62c3c
rcunico <rcunico@ggc.edu>
2019-04-11 12:14:38 -0400

    markers / trying CSVtoJSON (commented out)

--- src/app/comp-agm-map/comp-agm-map.component.ts
+++ src/app/comp-agm-map/comp-agm-map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { AgmMap} from '@agm/core';
 import { google } from '@agm/core/services/google-maps-types';
 import { Marker } from './marker';
+// import fs from 'fs';
 
 // declare let google: any;
 
@@ -14,27 +15,49 @@ import { Marker } from './marker';
 export class CompAgmMapComponent implements OnInit {
 
   @ViewChild('AgmMap') agmMap: AgmMap;
-  latitude: number = 33.980212;
-  longitude: number = -84.004298;
+  latitude = 33.980212;
+  longitude = -84.004298;
   locationChosen = false;
   newMarker: any;
   newMarker2 = { id: null, lat: null, lng: null};
   marker: Marker[] = [{id: 1, lat: 33.980212, lng: -84.006298}, {id: 2, lat: 33.980212, lng: -84.007298},
     {id: 3, lat: 33.980212, lng: -84.005298}];
-  // addMarker(event) {
-  //   this.lat = event.coords.lat;
-  //   this.lng = event.coords.lng;
-  //   this.locationChosen = true;
-  // }
+  // const csvtojsonV2 = require('csvtojson');
+  // csvtojsonV2 = require('csvtojson/v2');
 
-  ngOnInit() {
+ngOnInit() {
     this.createMarkers();
+    // this.createJSON();
   }
-  createMarkers(): void {
+
+
+createMarkers(): void {
     this.newMarker = JSON.parse('{"id": 1, "lat": 33.979611, "lng": -84.004950}');
     this.newMarker2.lat = Number(this.newMarker.lat);
     this.newMarker2.lng = Number(this.newMarker.lng);
   }
+  // async createJSON(): Promise<void> {
+    // const csvToJson = require('convert-csv-to-json');
+    // const fileInputName = 'myInputFile.csv';
+    // const fileOutputName = 'myOutputFile.json';
+    // csvToJson.generateJsonFileFromCsv('testdata.csv', 'testdata.json');
+    // const csvFilePath = './testdata.csv';
+    // const csv = require('csvtojson');
+    // csv()
+    //   .fromFile(csvFilePath)
+    //   .then((jsonObj) => {
+    //     console.log(jsonObj);
+        /**
+         * [
+         *  {a:"1", b:"2", c:"3"},
+         *  {a:"4", b:"5". c:"6"}
+         * ]
+         */
+      // });
+
+// Async / await usage
+//     const jsonArray = await csv().fromFile(csvFilePath);
+//   }
 }
   // ngAfterViewInit() {
   //   //let bounds = new google.maps.LatLngBounds();
